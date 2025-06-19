56d90f44756812c96560cbfe8997a9d8d5516a67
lokesh-patil57 <luckypatil577R@gmail.com>
2025-06-19 11:53:58 +0530

    Switch case in Java

---
+++ switchh.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class switchh {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int button = sc.nextInt();
+        switch(button){
+            case 1: System.err.println("HELLO");
+            break;
+            case 2: System.err.println("Welcome");
+            break;
+            case 3: System.err.println("Namaste");
+            break;
+        }
+    }    
+}
