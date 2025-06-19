aa82dbdf40773ccda38dfdfc396a56bff4314b58
lokesh-patil57 <luckypatil577R@gmail.com>
2025-06-19 11:50:20 +0530

    if else in java

---
+++ ifelse.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class ifelse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if (a%2==0) {
+            System.err.println("No is even");
+        } else {
+            System.err.println("No is ODD");
+        }
+    }
+    
+}
