bfdd579edff8c13f02321ff66322aa38652f4c4c
lokesh-patil57 <luckypatil577R@gmail.com>
2025-06-19 11:33:46 +0530

    ok

---
+++ sum.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class sum {
+
+    public static void main(String[] var0) {
+        Scanner var1 = new Scanner(System.in);
+        int var2 = var1.nextInt();
+        int var3 = var1.nextInt();
+        int var4 = var2 + var3;
+        System.out.println(var4);
+    }
+
+}
