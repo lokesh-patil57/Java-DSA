a871cf6142ff1128510e10662051e379a6791767
lokesh-patil57 <luckypatil577R@gmail.com>
2025-06-19 12:13:14 +0530

    while loop in java

---
+++ whileloop.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class whileloop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = 0; // Initialize a counter variable
+
+        // The loop continues as long as 'count' is less than 5
+        while (count < 5) { 
+            System.out.println("Current count: " + count);
+            count++; // Increment the counter in each iteration
+        }
+
+        
+    }
+}
