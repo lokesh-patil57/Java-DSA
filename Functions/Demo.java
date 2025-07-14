ea2886ceabfd823174ebb9c99016f22f88119877
Lucky Patil <luckypatil577R@gmail.com>
2025-07-14 23:19:54 +0530

    functions

---
+++ Functions/Demo.java
@@ -0,0 +1,10 @@
+package Functions;
+
+public class Demo{
+    public static void hello () {
+        System.out.println("Hello World");
+    }
+    public static void main(String[] args) {
+        hello();
+    }
+}
