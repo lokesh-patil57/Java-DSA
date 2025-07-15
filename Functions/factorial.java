b4e426b8a3ea2f97f5b24674e4ac3feeb68f02ef
Lucky Patil <luckypatil577R@gmail.com>
2025-07-15 23:57:28 +0530

    factorial function

---
+++ Functions/factorial.java
@@ -0,0 +1,21 @@
+package Functions;
+
+import java.util.Scanner;
+
+public class factorial {
+    public static int fact(int a) {
+        int fact = 1;
+        for(int i=1 ; i<=a ; i++){
+            fact = fact*i ;
+        }
+        return fact;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int fact  = fact(a);
+        System.out.println("Factorial is :"+fact);
+        sc.close();
+    }
+}
