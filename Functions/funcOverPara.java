649d1f4abf95be65289d2245d9afb74e683f1104
Lucky Patil <luckypatil577R@gmail.com>
2025-07-16 21:48:41 +0530

    function overloading with parameter

---
+++ Functions/funcOverPara.java
@@ -0,0 +1,22 @@
+package Functions;
+import java.util.Scanner;
+
+public class funcOverPara {
+    public static int sum(int a,int b) {
+        return a+b;
+    }
+    public static float sum (float a, float b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sum(5, 1);
+        float b = sum(3.3f, 2.7f);
+        System.out.println(a);
+        System.out.println(b);
+        
+
+
+        sc.close();
+    }
+}
