a3d36be2625eae6a399b2cb84d29148f635e7dde
Lucky Patil <luckypatil577R@gmail.com>
2025-07-18 00:30:40 +0530

    average of three no

---
+++ Functions/HW/AvgThree.java
@@ -0,0 +1,18 @@
+package Functions.HW;
+import java.util.Scanner;
+
+public class AvgThree {
+    public static int avgThree(int a, int b, int c) {
+        int avg = (a+b+c)/3;
+        return avg;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int avg = avgThree(a, b, c);
+        System.out.println("Average of no is : "+avg);
+        sc.close();
+    }
+}
