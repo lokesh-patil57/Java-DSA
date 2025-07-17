598d63375d3f16c50bc5ff22586f2879669ecb87
Lucky Patil <luckypatil577R@gmail.com>
2025-07-18 00:45:57 +0530

    moved files

--- Loops/forloop.java
+++ Loops/forloop.java
@@ -1,3 +1,4 @@
+package Loops;
 
 public class forloop {
     public static void main(String[] args) {
--- Loops/hello.java
+++ Loops/hello.java
@@ -1,3 +1,4 @@
+package Loops;
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
