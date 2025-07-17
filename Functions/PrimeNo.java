7bbfddd585f812197fa5ea6f52f633f41306f06f
Lucky Patil <luckypatil577R@gmail.com>
2025-07-17 19:49:33 +0530

    prime in range

--- Functions/PrimeNo.java
+++ Functions/PrimeNo.java
@@ -1,37 +1,48 @@
 package Functions;
+
 import java.util.Scanner;
 
 public class PrimeNo {
     public static boolean isPrime(int a) {
         boolean isPrime = true;
-        for(int i=2;i<=a-1 ;i++) {
-            if (a % i ==0) {
+        for (int i = 2; i <= a - 1; i++) {
+            if (a % i == 0) {
                 isPrime = false;
                 break;
             }
         }
         return isPrime;
     }
+
     public static boolean opPrime(int a) {
-        if (a==2) {
+        if (a == 2) {
             return true;
-        } else if (a==0||a==1) {
+        } else if (a == 0 || a == 1) {
             return false;
         }
-        for(int i=2; i<= Math.sqrt(a) ; i++) {
-            if (a%i==0) {
+        for (int i = 2; i <= Math.sqrt(a); i++) {
+            if (a % i == 0) {
                 return false;
             }
         }
         return true;
     }
 
+    public static void primesInRange(int a) {
+        for (int i = 2; i <= a; i++) {
+            if (opPrime(i)) {
+                System.out.print(i + " ");
+
+            }
+           
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        boolean prime = opPrime(a);
-        System.out.println("the given on is prime : "+prime);
-        
+        primesInRange(a);
+  
         sc.close();
     }
 }
