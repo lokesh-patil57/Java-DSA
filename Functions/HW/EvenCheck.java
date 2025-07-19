fbe3bb48fa72471e6269819f10619da3b406b0b3
Lucky Patil <luckypatil577R@gmail.com>
2025-07-19 11:13:21 +0530

    Even no check

--- Functions/HW/EvenCheck.java
+++ Functions/HW/EvenCheck.java
@@ -1,7 +1,6 @@
 //  Write a method named isEven that accepts an int argument. The method
 //  should return true if the argument is even, or false otherwise. Also write a program to test your
 //  method
-package Functions.HW;
 import java.util.Scanner;
 
 public class EvenCheck {
