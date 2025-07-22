8ef3f1c9f683af556a9f2e586fb7beac416c51d2
Lucky Patil <luckypatil577R@gmail.com>
2025-07-22 16:43:23 +0530

    call by reference

--- Arrays/ArrayArgum.java
+++ Arrays/ArrayArgum.java
@@ -1,15 +1,19 @@
 package Arrays;
 
 public class ArrayArgum {
-    public static void update(int marks[]) {
+    public static void update(int marks[], int nonchangable) {
+        nonchangable = 10;
         for (int i = 0; i < marks.length; i++) {
             marks[i] += 1;
         }
     }
     public static void main(String[] args) {
         int marks[] = {94,95,96};
-        update(marks);
+        // call by referance
+        int nonchangable = 5;
+        update(marks , nonchangable);
         //print out update marks
+        System.out.println(nonchangable);
         for (int i = 0; i < marks.length; i++) {
             System.out.println(marks[i]+" ");
         }
