8f44890143ff831c38b9ea943e3443a487199b69
Lucky Patil <luckypatil577R@gmail.com>
2025-07-23 14:57:05 +0530

    Linear Search , time Complexity: O(n)

---
+++ Arrays/LinearSearch.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+public class LinearSearch {
+    public static int linearSearch(int numbers[], int key) {
+        for (int i = 0; i < numbers.length; i++) {
+            if(numbers[i]==key) {
+                return i ;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int numbers[] = {1,2,3,4,5,6,7,8,9,10,11,12};
+        int key = 9;
+
+        int result = linearSearch(numbers, key);
+        if (result==-1) {
+            System.out.println("Key NOT found");
+        } else {
+            System.out.println("Key found at index:"+result);
+        }
+    }
+}
