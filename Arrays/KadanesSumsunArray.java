f580a998d3d067d61c208d9181326c777f1dd2f4
Lucky Patil <luckypatil577R@gmail.com>
2025-07-23 23:50:13 +0530

    Maximum sum of sub arrays by kadanes algo TC :- O(n)

---
+++ Arrays/KadanesSumsunArray.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+public class KadanesSumsunArray {
+    public static void kadanes(int numbers[]) {
+        int maxSum = Integer.MIN_VALUE;
+        int curSum = 0;
+        for (int i = 1; i < numbers.length; i++) {
+            curSum = curSum + numbers[i];
+            if (curSum<0) {
+                curSum =0;
+            }
+            maxSum = Math.max(maxSum, curSum);
+        }
+        System.out.println("Max sum :"+maxSum );
+    }
+    public static void main(String[] args) {
+        int numbers[] = {-2,-3,4,-1,-2,1,5,-3};
+        kadanes(numbers);
+    }
+}
