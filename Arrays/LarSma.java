38c8ed2a356b201f4cc841ddc3a8a39329fe6612
Lucky Patil <luckypatil577R@gmail.com>
2025-07-23 15:20:26 +0530

    LArgest and smallest in Array

---
+++ Arrays/LarSma.java
@@ -0,0 +1,23 @@
+package Arrays;
+// import java.util.Scanner;
+
+public class LarSma {
+    public static int getLargestSmallest(int numbers[]) {
+        int largest = Integer.MIN_VALUE;
+        int smallest = Integer.MAX_VALUE;
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (largest <numbers[i]) {
+                largest = numbers[i];
+            } else if(smallest > numbers[i]) {
+                smallest = numbers[i];
+            }
+        }
+        System.out.println("Smallest is : "+ smallest);
+        return largest;
+    }
+    public static void main(String[] args) {
+        int numbers[] ={1 ,3, 5 ,7,10, 2 , 4 ,6 };
+        System.out.println("Largest of numbers : "+getLargestSmallest(numbers));
+    }
+}
