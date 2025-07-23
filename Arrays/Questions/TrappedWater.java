4333ce5b4d928f574a8773228ca287943b39d168
Lucky Patil <luckypatil577R@gmail.com>
2025-07-24 00:52:05 +0530

    Trapped Water problem TC:- O(n)

---
+++ Arrays/Questions/TrappedWater.java
@@ -0,0 +1,34 @@
+package Arrays.Questions;
+
+public class TrappedWater {
+    public static int trappedWater(int height[]) {
+        int n = height.length;
+        //calculate leftMax boundary - HelperArray
+        int leftMax[] = new int[n];
+        leftMax[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(height[i], leftMax[i-1]) ;
+        }
+        //calculate RightMax boundary - HelperArray
+        int rightMax[] = new int[n];
+        rightMax[n-1] = height[n-1];
+        for (int i = n-2; i >= 0; i--) {
+            rightMax[i] = Math.max(height[i], rightMax[i+1]);
+        }
+        //loop
+        int trappedWater = 0;
+        for (int i = 0; i < n; i++) {
+            //waterLevel = min(leftMax,rightMax)
+            int waterLevel = Math.min(rightMax[i], leftMax[i]);
+
+            //TrappedWater = (waterLevel - height ) * width of bar
+            trappedWater += waterLevel - height[i];
+        }
+        return trappedWater;
+    }
+    public static void main(String[] args) {
+        int height[] =  { 4,2,0,6,3,2,5};
+        int result = trappedWater(height) ;
+        System.out.println("Trapped Water :"+result);
+    }
+}
