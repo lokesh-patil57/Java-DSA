51a322147f3fc6e7a1663aea20eef079ee05716d
Lucky Patil <luckypatil577R@gmail.com>
2025-07-31 12:44:37 +0530

    Clear I range of bit

---
+++ BitManupulation/ClearIRangeBit.java
@@ -0,0 +1,13 @@
+package BitManupulation;
+
+public class ClearIRangeBit {
+    public static int clearIRangeBit (int n ,int i,int j)  {
+        int a = ~0<<j+1;
+        int b = (1<<i)-1;
+        int bitMask = a|b;
+        return n&bitMask;
+    }
+    public static void main(String[] args) {
+        System.out.println(clearIRangeBit(10, 2, 4));
+    }
+}
