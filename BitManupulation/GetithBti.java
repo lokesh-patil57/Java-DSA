954d97a5ef0a2f4a0198d09e5e5f300e8c15cc33
Lucky Patil <luckypatil577R@gmail.com>
2025-07-31 11:41:08 +0530

    to Get Ith bit

---
+++ BitManupulation/GetithBti.java
@@ -0,0 +1,14 @@
+package BitManupulation;
+
+public class GetithBti {
+    public static int getIthBit(int n , int i) {
+        int bitMask = 1<<i;
+        if ((n & bitMask )== 0) {
+            return 0;
+        }
+        return 1;
+    }
+    public static void main(String[] args) {
+        System.out.println(getIthBit(10, 2));
+    }
+}
