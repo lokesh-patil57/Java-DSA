b2fd90949c9b839e7de50c05dd63b03f9bb16f3d
Lucky Patil <luckypatil577R@gmail.com>
2025-08-04 23:27:57 +0530

    Inheritance in java

---
+++ OOPs/Inheritance.java
@@ -0,0 +1,22 @@
+package OOPs;
+
+public class Inheritance {
+    public static void main(String[] args) {
+        fish shark = new fish();
+        shark.eat();
+        
+    }
+
+}
+
+class Animal {
+    String color;
+
+    void eat() {
+        System.out.println("Eats");
+    }
+    void breath() {
+        System.out.println("breaths");
+    }
+}
+
