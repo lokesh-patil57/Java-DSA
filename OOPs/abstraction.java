2e810a3f37f216ec77101ecb4a82157306cec894
Lucky Patil <luckypatil577R@gmail.com>
2025-08-07 22:36:01 +0530

    Abstraction in oop

---
+++ OOPs/abstraction.java
@@ -0,0 +1,18 @@
+package OOPs;
+
+public class abstraction {
+    public static void main(String[] args) {
+        
+    }
+}
+
+abstract class Animal{
+    String color;
+    Animal(){
+        color="Brown";
+    }
+    void eat(){
+        System.out.println("Animal eats");
+    }
+    abstract void walk();
+}
