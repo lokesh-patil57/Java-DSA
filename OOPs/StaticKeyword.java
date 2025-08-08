8d80205cbb84ad33a488465d18a76aa6371e1ab3
Lucky Patil <luckypatil577R@gmail.com>
2025-08-09 00:45:30 +0530

    Student class containing static schoolname

--- OOPs/StaticKeyword.java
+++ OOPs/StaticKeyword.java
@@ -3,6 +3,10 @@ package OOPs;
 
 public class StaticKeyword {
     public static void main(String[] args) {
-    
+        Student s1 = new Student();
+        s1.schoolname = "PVC";
+        Student S2 =  new Student();
+        System.out.println(S2.schoolname);
     }
-}
+
+ 
