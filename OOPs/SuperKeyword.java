d7d0465ddff6a36b357d64b4923f7efbf54c107c
Lucky Patil <luckypatil577R@gmail.com>
2025-08-09 00:54:48 +0530

    Super Keyword

---
+++ OOPs/SuperKeyword.java
@@ -0,0 +1,15 @@
+package OOPs;
+
+public class SuperKeyword {
+    public static void main(String[] args) {
+        
+    }
+}
+
+class Animal{
+    String color;
+    Animal() {
+        System.out.println("Animal Constructor is Called");
+    }
+}
+
