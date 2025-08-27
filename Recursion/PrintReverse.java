3784672bb5301b38ddd7cce193df908cd9949a98
Lucky Patil <luckypatil577R@gmail.com>
2025-08-27 23:40:43 +0530

    Print Reverse numbers

--- Recursion/PrintReverse.java
+++ Recursion/PrintReverse.java
@@ -1,7 +1,14 @@
 package Recursion;
 
 public class PrintReverse {
-    
+    public static void printRev(int n) {
+        if (n == 1) {
+            System.out.println(n);
+            return;
+        }
+        System.out.print(n + " ");
+        printRev(n - 1);
+    }
 
     public static void main(String[] args) {
         int n = 10;
