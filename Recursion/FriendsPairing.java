2857706a18892fddebf37a3084f91fbbc0ee27ee
Lucky Patil <luckypatil577R@gmail.com>
2025-08-28 14:56:16 +0530

    Friends Pairing Goldman Sacs

---
+++ Recursion/FriendsPairing.java
@@ -0,0 +1,20 @@
+package Recursion;
+
+public class FriendsPairing {
+    public static int friendsPairing(int n) {
+        if (n==1||n==2) {
+            return n;
+        }
+        // Choices
+        // Single
+        int fnm1 = friendsPairing(n-1);
+        // Pair
+        int fnm2 = (n-1)*friendsPairing(n-2);
+        // Total
+        int totalWays = fnm1+fnm2;
+        return totalWays;
+    }
+    public static void main(String[] args) {
+        System.out.println(friendsPairing(3));
+    }
+}
