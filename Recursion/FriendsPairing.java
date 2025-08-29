0cfb42a285dc99200677f8c3fc1b7c607e8bf535
Lucky Patil <luckypatil577R@gmail.com>
2025-08-29 16:46:12 +0530

    Optimized friends pairing

--- Recursion/FriendsPairing.java
+++ Recursion/FriendsPairing.java
@@ -5,14 +5,16 @@ public class FriendsPairing {
         if (n==1||n==2) {
             return n;
         }
-        // Choices
-        // Single
-        int fnm1 = friendsPairing(n-1);
-        // Pair
-        int fnm2 = (n-1)*friendsPairing(n-2);
-        // Total
-        int totalWays = fnm1+fnm2;
-        return totalWays;
+        // // Choices
+        // // Single
+        // int fnm1 = friendsPairing(n-1);
+        // // Pair
+        // int fnm2 = (n-1)*friendsPairing(n-2);
+        // // Total
+        // int totalWays = fnm1+fnm2;
+        // return totalWays;
+
+        return friendsPairing(n-1) + (n-1)*friendsPairing(n-2);
     }
     public static void main(String[] args) {
         System.out.println(friendsPairing(3));
