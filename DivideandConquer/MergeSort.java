23233d0966ba2951bd4e98412fec7ae4176b72a0
Lucky Patil <luckypatil577R@gmail.com>
2025-08-29 18:40:55 +0530

    Mege arrays

--- DivideandConquer/MergeSort.java
+++ DivideandConquer/MergeSort.java
@@ -19,7 +19,41 @@ public class MergeSort {
         merge(arr, si, mid, ei);
     }
 
-    
+    public static void merge(int arr[], int si, int mid, int ei) {
+        int temp[] = new int[ei-si+1];
+        int i = si; // index for first sorted part 
+        int j = mid+1; // index for second sorted part
+        int k = 0; // index for temp array
+
+        //compairing the elements of left and right part
+        while (i <= mid && j<=ei) {
+            if (arr[i] < arr[j]) {
+                temp[k] = arr[i];
+                i++;  
+            }
+            else {
+                temp[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Reamining last element in Left part
+        while (i<=mid) {
+            temp[k++] = arr[i++];
+
+        }
+
+        // Reamining last element in Right part
+        while (j<=ei) {
+            temp[k++] = arr[j++];
+        }
+
+        // Copy the elements in temp array in Original Array
+        for ( k = 0 , i=si; k < temp.length; k++ , i++) {
+            arr[i] = temp[k];
+        }
+    }
 
     public static void main(String[] args) {
         int arr[] = { 6, 3, 9, 5, 2, 8 };
