f9c59d396d00d813b20775c17bc65f06ecfef621
Lucky Patil <luckypatil577R@gmail.com>
2025-08-29 23:55:38 +0530

    Quick Sort O(logn)

---
+++ DivideandConquer/QuickSort.java
@@ -0,0 +1,18 @@
+package DivideandConquer;
+
+public class QuickSort {
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void quickSort(int arr[], int si , int ei) {
+        
+    }
+    public static void main(String[] args) {
+        int arr[] = { 6, 3, 9, 5, 2, 8 };
+        quickSort(arr, 0, arr.length-1);
+        printArray(arr);
+    }
+}
