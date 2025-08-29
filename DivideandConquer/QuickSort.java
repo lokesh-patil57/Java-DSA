c76e33b56a6145dff1b4682527eb93f911832f86
Lucky Patil <luckypatil577R@gmail.com>
2025-08-30 00:00:01 +0530

    Quick Sort code

--- DivideandConquer/QuickSort.java
+++ DivideandConquer/QuickSort.java
@@ -7,12 +7,27 @@ public class QuickSort {
         }
         System.out.println();
     }
-    public static void quickSort(int arr[], int si , int ei) {
-        
+
+    public static void quickSort(int arr[], int si, int ei) {
+        if (si >= ei) {
+            return;
+        }
+        // LAst element
+        int pidx = partition(arr, si, ei);
+
+        // Quick sort for left
+        quickSort(arr, si, pidx - 1);
+        // Quick sort for left
+        quickSort(arr, pidx + 1, ei);
     }
+
+    public static int partition(int arr[], int si, int ei) {
+
+    }
+
     public static void main(String[] args) {
         int arr[] = { 6, 3, 9, 5, 2, 8 };
-        quickSort(arr, 0, arr.length-1);
+        quickSort(arr, 0, arr.length - 1);
         printArray(arr);
     }
 }
