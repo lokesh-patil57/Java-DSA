ce09a404362bf0c0e06bbe9deafd6f26f07a378b
Lucky Patil <luckypatil577R@gmail.com>
2025-08-31 23:44:30 +0530

    Permutaion complete

--- Backtracking/Permutations.java
+++ Backtracking/Permutations.java
@@ -2,9 +2,22 @@ package Backtracking;
 
 public class Permutations {
     public static void findPermutation(String str , String ans) {
+        //Base Case
+        if (str.length()==0) {
+            System.out.println(ans);
+            return;
+        }
 
+        // Recurssion
+        for (int i = 0; i < str.length(); i++) {
+            char currr =str.charAt(i);
+            //"abcde " => "ab"+"de" = "abde"
+            String Newstr = str.substring(0,i)+str.substring(i+1);
+            findPermutation(Newstr, ans+currr);
+        }
     }
     public static void main(String[] args) {
         String str = "abc";
+        findPermutation(str, "");
     }
 }
