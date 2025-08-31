81368eb77498b2a435b4ba8e3ec1710500c59ce8
Lucky Patil <luckypatil577R@gmail.com>
2025-08-31 23:30:26 +0530

    base case and Yes No case

--- Backtracking/findSubarrays.java
+++ Backtracking/findSubarrays.java
@@ -2,7 +2,17 @@ package Backtracking;
 
 public class findSubarrays {
     public static void FindSubArr(String str, String ans , int i){
-        
+        // base case 
+        if (i==str.length()) {
+            System.out.println(ans);
+            return;
+        }
+
+        // Yes Choice
+        FindSubArr(str, ans+str.charAt(i), i+1);
+
+        // No choice
+        FindSubArr(str, ans, i+1);
     }
     public static void main(String[] args) {
         
