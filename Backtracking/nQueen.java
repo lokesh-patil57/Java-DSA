e226bb6f8bbd37448c9f74a18e7c6dabaae8e62a
Lucky Patil <luckypatil577R@gmail.com>
2025-09-01 23:57:56 +0530

    input in Nqueens

--- Backtracking/nQueen.java
+++ Backtracking/nQueen.java
@@ -2,9 +2,18 @@ package Backtracking;
 
 public class nQueen {
     public static void nqueen(char board[][], int row) {
-
+        for (int i = 0; i < board.length; i++) {
+            
+        }
     }
     public static void main(String[] args) {
-        
+        int n = 2;
+        char board[][] = new char[n][n];
+        // initialize
+        for(int i=0;i<n;i++){
+            for(int j=0; j<n; j++){
+                board[i][j] = '.';
+            }
+        }
     }
 }
